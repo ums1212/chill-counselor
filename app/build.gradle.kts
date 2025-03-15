e9f399803d3893af678ea8e2c473411a50cd2c9a
ums1212 <voll1212@gmail.com>
2025-03-15 19:47:48 +0900

    [chore] 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "org.comon.chillcounselor"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 20250315
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
